edc0809854be3c581565b8c4a47215d2042e5ae7
Milena Krawczyk <mmilena.krawczyk@gmail.com>
2023-01-27 18:41:04 +0100

    Descriptions

--- frontend/pages/customer/Customer/Welcome/index.js
+++ frontend/pages/customer/Customer/Welcome/index.js
@@ -4,8 +4,8 @@ const Welcome = ({ ftMetadata, programIsActive, merchantAddress }) => {
   return (
     <WelcomeSection>
       {merchantAddress && 'Loyalty Program'}
-      {merchantAddress && (programIsActive ? ftMetadata.token_name : 'no loyalty program working...')}
-      {programIsActive || '...use the list below to choose Loyalty Program'}
+      {merchantAddress && (programIsActive ? ftMetadata.token_name : `You don't have a loyalty program yet`)}
+      {programIsActive || 'Log in as a merchant to activate the loyalty program. These are the programs already created:'}
     </WelcomeSection>
   );
 };
